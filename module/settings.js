a466d052ddbd35d656ded57d48840282803b70a7
Chris Schmalz <abjectfilm@gmail.com>
2023-09-16 15:56:05 -0700

    add damage dice setting

--- module/settings.js
+++ module/settings.js
@@ -56,59 +56,39 @@ export const registerSettings = function () {
           }
     });
 
-    // game.settings.register('mosh', 'newArmor', {
-    //     name: "1e Armor System",
-    //     hint: "Use the 1st edition Armor system.",
-    //     default: true,
-    //     scope: 'world',
-    //     type: Boolean,
-    //     config: true,
-    //     onChange: value => { // A callback function which triggers when the setting is changed
-    //         console.log("newArmorandCombat set to " + value)
-    //       }
-    // });
-    // game.settings.register('mosh', 'newStress', {
-    //     name: "1e Stress System",
-    //     hint: "Use the 1st edition stress system with the roll under d20 and stress table.",
-    //     default: true,
-    //     scope: 'world',
-    //     type: Boolean,
-    //     config: true,
-    //     onChange: value => { // A callback function which triggers when the setting is changed
-    //         console.log("newStress set to " + value)
-    //       }
-    // });
-    // game.settings.register('mosh', 'newWounds', {
-    //     name: "1e Wound System",
-    //     hint: "[Instead of the 0e health system, use the 1e wounds and health system.",
-    //     default: true,
-    //     scope: 'world',
-    //     type: Boolean,
-    //     config: true,
-    //     onChange: value => { // A callback function which triggers when the setting is changed
-    //         console.log("newWounds set to " + value)
-    //       }
-    // });
-    game.settings.register('mosh', 'hideWeight', {
-        name: "Hide Weight",
-        hint: "Hide weight in the items list for players and ships?",
-        default: false,
-        scope: 'world',
-        type: Boolean,
-        config: true,
-        onChange: value => { // A callback function which triggers when the setting is changed
-            console.log("hideWeight set to " + value)
-          }
-    });
-    game.settings.register('mosh', 'useCalm', {
-        name: "Use Calm?",
-        hint: "Uses the traaa.sh Calm system instead of Stress.",
-        default: false,
-        scope: 'world',
-        type: Boolean,
-        config: true,
-        onChange: value => { // A callback function which triggers when the setting is changed
-            console.log("useCalm set to " + value)
-          }
-    });
+    game.settings.register('mosh', 'damageDiceTheme', {
+      name: "Damage Dice Theme",
+      hint: "If DiceSoNice is installed, what theme should be applied to damage dice?",
+      default: "red",
+      scope: 'world',
+      type: String,
+      config: true,
+      onChange: value => { // A callback function which triggers when the setting is changed
+          console.log("Damage dice theme set to " + value)
+        }
+  });
+
+  game.settings.register('mosh', 'hideWeight', {
+      name: "Hide Weight",
+      hint: "Hide weight in the items list for players and ships?",
+      default: false,
+      scope: 'world',
+      type: Boolean,
+      config: true,
+      onChange: value => { // A callback function which triggers when the setting is changed
+          console.log("hideWeight set to " + value)
+        }
+  });
+  
+  game.settings.register('mosh', 'useCalm', {
+      name: "Use Calm?",
+      hint: "Uses the traaa.sh Calm system instead of Stress.",
+      default: false,
+      scope: 'world',
+      type: Boolean,
+      config: true,
+      onChange: value => { // A callback function which triggers when the setting is changed
+          console.log("useCalm set to " + value)
+        }
+  });
 };

76baacff961248c6fbe95f3fe9ab6e7c1ed668ec
Chris Schmalz <abjectfilm@gmail.com>
2023-09-18 19:01:41 -0700

    near final triggered macros

---
+++ macros/triggered/-1d5_max_health.js
@@ -0,0 +1,15 @@
+prepModifyActor('system.health.max',null,'-1d5',true);
+
+//tell the actor to roll on the table
+async function prepModifyActor(fieldAddress,modValue,modRollString,outputChatMsg) {
+    //determine who to run the macro for
+    if (game.settings.get('mosh','macroTarget') === 'character') {
+      //roll the table for the player's 'Selected Character'
+      game.user.character.modifyActor(fieldAddress,modValue,modRollString,outputChatMsg);
+    } else if (game.settings.get('mosh','macroTarget') === 'token') {
+      //roll the table for all selected tokens
+      canvas.tokens.controlled.forEach(function(token){
+        token.actor.modifyActor(fieldAddress,modValue,modRollString,outputChatMsg);
+      });
+    }
+}
